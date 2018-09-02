1542338295add98db605e79459e452b6d5233388
thunder <thundercumt@126.com>
2018-09-02 18:48:27 +0800

    basic apriori algorithm to return frequency item set

--- datawin-py/mine.py
+++ datawin-py/mine.py
@@ -16,14 +16,14 @@ class Dao(object):
 
     def load_all(self):
         cursor = self.db.cursor()
-        sql = "select * from fc_project_tags limit 100000 offset 0 "
+        sql = "select * from fc_project_tags limit 1000000 offset 0 "
         try:
             count = cursor.execute(sql)
             ret = []
             results = cursor.fetchall()
             for row in results:
                 ret.append(row)
-            print("%d records occupied %d bytes" % (count, sys.getsizeof(ret),))
+            # print("%d db records occupied %d bytes" % (count, sys.getsizeof(ret),))
             return ret
         except Exception as e:
             raise e
@@ -35,28 +35,6 @@ class AssociationRule(object):
 
     def __init__(self, rows):
         self.rows = rows
-
-    def support_set(data, cand_set, kth, threshold):
-        minimum = int(threshold * len(data))
-        d = dict()
-        next_cand_set = set()
-
-        print(kth)
-        cand_tuples = itertools.combinations(cand_set, kth)
-        for c in cand_tuples:
-            for k, v in data.items():
-                # k is basket, v is purchases
-
-                if set(c).issubset(v):
-                    if c in d:
-                        d[c] += 1
-                    else:
-                        d[c] = 1
-
-            if c in d and d[c] > minimum:
-                next_cand_set = next_cand_set | set(c)
-
-        return {k: v for k, v in d.items() if v > minimum}, next_cand_set
 
     def process_dataset(self):
         a = dict()
@@ -70,15 +48,55 @@ class AssociationRule(object):
                 b[r[1]] = 1
             else:
                 b[r[1]] += 1
+
         return b, {k: tuple(v) for k, v in a.items()}
 
-    def apriori_fn(self, support=0.05):
+    def support_set(data, cand_set, kth, min_support):
+        d = dict()
+        next_cand_set = set()
+        next_data = dict()
+
+        print(kth)
+        cand_tuples = itertools.combinations(cand_set, kth)
+        for c in cand_tuples:
+            for k, v in data.items():
+                # k is basket, v is purchases
+
+                # optimize when kth is 1, in which case c is single-element set
+                if kth == 1:
+                    if c[0] in v:
+                        if c in d:
+                            d[c] += 1
+                        else:
+                            d[c] = 1
+
+                else:
+                    if set(c).issubset(v):
+                        if c in d:
+                            d[c] += 1
+                        else:
+                            d[c] = 1
+
+            if c in d and d[c] > min_support:
+                next_cand_set = next_cand_set.union(set(c))
+
+        for k, v in data.items():
+            if len(v) > kth and len(next_cand_set.intersection(v)) >= kth:
+                next_data[k] = v
+
+        return {k: v for k, v in d.items() if v > min_support}, next_cand_set, next_data
+
+    def apriori(self, support=0.05):
         tags, projects = self.process_dataset()
+        min_support = int(support * len(projects))
+
+        print('min support', min_support)
 
         cands = tags.keys()
+        next_data = projects
         kth = 1
         while True:
-            item_set, cands = AssociationRule.support_set(projects, cands, kth, support)
+            item_set, cands, next_data = AssociationRule.support_set(next_data, cands, kth, min_support)
             if len(cands) == 0:
                 break
 
@@ -91,4 +109,4 @@ if __name__ == '__main__':
     db = Dao()
     data = db.load_all()
     ar = AssociationRule(data)
-    ar.apriori_fn()
+    ar.apriori(0.05)
